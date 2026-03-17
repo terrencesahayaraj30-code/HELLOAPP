23518008480784b83402497c96d89993c1fb88ef
TERRENCE <terrencesahayaraj30@gmail.com>
2026-03-17 21:16:53 +0530

    Add files via upload

---
+++ HELLOAppUC4.java
@@ -0,0 +1,19 @@
+public class HELLOAppUC4 {
+
+    public static void main(String[] args) {
+
+        String name = "Guest";
+
+        if (args.length > 0) {
+            StringBuilder nameBuilder = new StringBuilder();
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+            name = nameBuilder.toString();
+        }
+        System.out.println("Hello " + name);
+    }
+}
