d6db65acb922dd95a49bb619e79ea96b05228b4b
TERRENCE <terrencesahayaraj30@gmail.com>
2026-03-18 13:10:18 +0530

    Add files via upload

---
+++ HELLOAppUC6.java
@@ -0,0 +1,14 @@
+public class HELLOAppUC6 {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+        StringBuilder nameBuilder = new StringBuilder();
+        for (String name : args) {
+            nameBuilder.append(name).append(", ");
+        }
+        String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+        System.out.println("Hello, " + names + "!");
+    }
+}
